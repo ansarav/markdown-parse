5811436e665f6c26d305e0cd4a9d42a55fd4b0fb
ansarav <ansaravi@ucsd.edu>
2022-01-28 05:15:55 -0800

    Yayyy  return empty when empty
    The bug was that although that the current index was less than markdown.length, so it did go through looking for the characters [,],(,) , but since it didn't find it would add nothing so the array was as if it din't exist. When it did it was simply empty. (Symptom).  It wouldn't have been caught if it wasn't for the failure-induced input but it allowed us to go back and to the .java file and consider  to code the empty array before trying to add an element that was not present. Because the condition was true it kept looping and looping but since no elements were added, there was no way to escape the loop and thus never returned the array "toReturn". So by writing an if statement in the middle it allowed efficient fdebugging

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -8,15 +8,22 @@ public class MarkdownParse {
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then take up to
-        // the next )
-        int currentIndex = 0;
+        // the next )  
+       
+        int currentIndex = 0;      
+        
         while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
+
+            if(nextOpenBracket == -1 || nextCloseBracket == -1 || openParen == -1 || closeParen == -1){
+                return toReturn;
+            }
+
             toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
+            currentIndex = closeParen + 1;           
         }
         return toReturn;
     }
