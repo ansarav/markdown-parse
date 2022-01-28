5fccdd871c8b9101e61e2388ab42de68f7151c2f
ansarav <ansaravi@ucsd.edu>
2022-01-28 10:58:12 -0800

    Exception in thread "main" java.lang.StringIndexOutOfBoundsException: begin 0, end -1, length 11         at java.base/java.lang.String.checkBoundsBeginEnd(String.java:3751)         at java.base/java.lang.String.substring(String.java:1907)         at MarkdownParse.getLinks(MarkdownParse.java:27)         at MarkdownParse.main(MarkdownParse.java:42)
    Exception in thread "main" java.lang.StringIndexOutOfBoundsException: begin 0, end -1, length 11
            at java.base/java.lang.String.checkBoundsBeginEnd(String.java:3751)
            at java.base/java.lang.String.substring(String.java:1907)
            at MarkdownParse.getLinks(MarkdownParse.java:27)
            at MarkdownParse.main(MarkdownParse.java:42)

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -15,9 +15,13 @@ public class MarkdownParse {
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);    
 
-            if(nextOpenBracket == -1 || nextCloseBracket == -1 || openParen == -1 || closeParen == -1){
+           /*  if(nextOpenBracket == -1 || nextCloseBracket == -1 || openParen == -1 || closeParen == -1){
                 return toReturn;
-            }  
+            }   */
+
+           /*  if(nextOpenBracket == -1 && nextCloseBracket == -1 && openParen == -1 && closeParen == -1){
+                return toReturn;
+            }   */
 
             //System.out.println(markdown.substring(nextOpenBracket +1, nextCloseBracket));
             String KeyWord = markdown.substring(nextOpenBracket +1, nextCloseBracket);
