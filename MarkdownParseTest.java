c40cabd1e1d2e7c2268103897f15b5cc729d033c
ansarav <76548988+ansarav@users.noreply.github.com>
2022-03-11 13:07:04 -0800

    remote ieng6

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,46 +1,31 @@
-
-
 import static org.junit.Assert.*;
-
+import org.junit.*;
+import java.util.*;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 
-import org.junit.*;1) testSnippet1(MarkdownParseTest)
-java.lang.AssertionError: expected:<[`google.com, google.com, ucsd.edu]> but was:<[url.com, `google.com, google.com]>
+public class MarkdownParseTest {
+                            @Test
+                                public void testSnippet1() throws IOException {
+                                                String regFile = Files.readString(Path.of("./snippet-1.md"));
+                                                        String[] regLines = regFile.split("\n");
+                                                                assertEquals(List.of("`google.com", "google.com", "ucsd.edu"), MarkdownParse.getLinks(regLines));        
+                                                                    }
 
-    @Test
-    public void testImageDistinction() throws IOException {
-        ArrayList<String> Image = new ArrayList<>();
-        Image.add("a-link.html");
-        String ImageTest = MarkdownParse.converter("image-distinction.md");
-        assertEquals(Image, MarkdownParse.getLinks(ImageTest));
-    }
+                                @Test
+                                    public void testSnippet2() throws IOException {
+                                                    String regFile = Files.readString(Path.of("./snippet-2.md"));
+                                                            String[] regLines = regFile.split("\n");
+                                                                    assertEquals(List.of("a.com", "a.com(())", "example.com"), MarkdownParse.getLinks(regLines));        
+                                                                }
 
-    @Test
-    public void testSnippet1() throws IOException {
-        ArrayList<String> SnippetExpected = new ArrayList<>();
-        SnippetExpected.add("`google.com");
-        SnippetExpected.add("google.com");
-        SnippetExpected.add("ucsd.edu");
-        String SnippetActual = MarkdownParse.converter("Snippet1.md");
-        assertEquals(SnippetExpected, MarkdownParse.getLinks(SnippetActual));
-    }
 
-    @Test
-    public void testSnippet2() throws IOException {
-        ArrayList<String> SnippetExpected = new ArrayList<>();
-        SnippetExpected.add("b.com");
-        SnippetExpected.add("a.com(())");
-        SnippetExpected.add("example.com");
-        String SnippetActual = MarkdownParse.converter("Snippet2.md");
-        assertEquals(SnippetExpected, MarkdownParse.getLinks(SnippetActual));
-    }
-
-    @Test
-    public void testSnippet3() throws IOException {
-        ArrayList<String> SnippetExpected = new ArrayList<>();
-        SnippetExpected.add("https://ucsd-cse15l-w22.github.io/");
-        String SnippetActual = MarkdownParse.converter("Snippet3.md");
-        assertEquals(SnippetExpected, MarkdownParse.getLinks(SnippetActual));
-    }
-    }
+                                    @Test
+                                        public void testSnippet3() throws IOException {
+                                                        String regFile = Files.readString(Path.of("./snippet-3.md"));
+                                                                String[] regLines = regFile.split("\n");
+                        assertEquals(List.of("https://www.twitter.com", "https://ucsd-cse15l-w22.github.io/", "https://cse.ucsd.edu/"), MarkdownParse.getLinks(regLines));
+          }
+}
